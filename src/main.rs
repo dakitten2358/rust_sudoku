4abcd07d63e0dad09ad46ec2340feee7968b4e45
dakitten2357 <jay.w.murray@gmail.com>
2017-07-13 09:58:38 -0400

    ch2 complete - panics :(

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,15 @@
 extern crate piston;
+extern crate glutin_window;
 
+use piston::event_loop::{Events, EventLoop, EventSettings};
 use piston::window::WindowSettings;
+use glutin_window::GlutinWindow;
 
 fn main() {
 	let settings = WindowSettings::new("Sudoku", [512;2]).exit_on_esc(true);
-	println!("{}", settings.get_exit_on_esc());
+	let mut window: GlutinWindow = settings.build().expect("failed to create window");
+	let mut events = Events::new(EventSettings::new().lazy(true));
+	while let Some(e) = events.next(&mut window) {
+
+	}
 }
