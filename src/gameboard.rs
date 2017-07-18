0f21b78a1595c5e88f5467a8ccf389d2b6259292
dakitten2357 <jay.w.murray@gmail.com>
2017-07-18 16:25:08 -0400

    added ability to solve a sudoku

--- src/gameboard.rs
+++ src/gameboard.rs
@@ -35,4 +35,86 @@ impl Gameboard {
     pub fn set_cell(&mut self, ind: [usize; 2], val: u8) {
         self.cells[ind[1]][ind[0]] = val;
     }
+
+    fn is_valid(&self, ind:[usize; 2], val:u8) -> bool {
+        /// check to see if we already have this number in the given row or column
+        for i in 0..9 {
+            if (self.cells[i][ind[0]] == val) {
+                return false;
+            }
+            if (self.cells[ind[1]][i] == val) {
+                return false;
+            }
+        }
+
+        // check the remaining four spaces (tl, tr, bl, br) in this section
+        let section_row = 3 * (ind[1] / 3);
+        let section_col = 3 * (ind[0] / 3);
+
+        let row1 = (ind[1]+2) % 3;
+        let row2 = (ind[1]+4) % 3;
+
+        let col1 = (ind[0]+2) % 3;
+        let col2 = (ind[0]+4) % 3;
+
+        if (self.cells[row1 + section_row][col1 + section_col] == val) {
+            return false;
+        }
+        if (self.cells[row2 + section_row][col1 + section_col] == val) {
+            return false;
+        }
+
+        if (self.cells[row1 + section_row][col2 + section_col] == val) {
+            return false;
+        }
+        if (self.cells[row2 + section_row][col2 + section_col] == val) {
+            return false;
+        }
+
+
+        true
+    }
+
+    /// solve a gameboard
+    pub fn solve(&mut self, ind:[usize; 2]) -> bool {
+        // make sure we exit if we're done
+        if SIZE == ind[1] {
+            return true;
+        }
+
+        // skip cells that already have a value
+        if self.cells[ind[1]][ind[0]] > 0 {
+            if ind[0] == (SIZE-1) {
+                if self.solve([0, ind[1] + 1]) {
+                    return true;
+                }
+            }
+            else {
+                if self.solve([ind[0] + 1, ind[1]]) {
+                    return true;
+                }
+            }
+        }
+
+        for next_val in 1..10 {
+            if self.is_valid(ind, next_val) {
+                self.set_cell(ind, next_val);
+
+                if ind[0] == (SIZE-1) {
+                    if self.solve([0, ind[1] + 1]) {
+                        return true;
+                    }
+                }
+                else {
+                    if self.solve([ind[0] + 1, ind[1]]) {
+                        return true;
+                    }
+                }
+
+                self.set_cell(ind, 0);
+            }
+        }
+
+        false
+    }
 }
